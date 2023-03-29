0d4e176d07ca35d48bf310bb7357095b31bcbfd2
Konrad Baumann <baumannkonrad@outlook.de>
2023-03-29 19:19:42 -0400

    Adds Pages / Modifies navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,33 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
   def home
+    @events = Event.all
   end
+
+  def event
+    @event = Event.find(params[:id])
+  end
+
+  def my_events
+    @reservations = current_user.reservations
+  end
+
+  def new_reservation
+    reservation = Reservation.new
+    reservation.user = current_user
+    reservation.event = Event.find(params[:id])
+    reservation.save!
+  end
+  helper_method :new_reservation
+
+  def check_reservation(event)
+    has_reservation = false
+    reservations = Reservation.all
+    reservations.each do |reservation|
+      if reservation.user == current_user && reservation.event == event
+        has_reservation = true
+      end
+    end
+    return has_reservation
+  end
+  helper_method :check_reservation
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "pages#home"
+
+  get "my_events", to: "pages#my_events"
+
+  get "events/:id", to: "pages#event"
+
+  get "events/:id/reservations/new", to: "pages#new_reservation"
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
