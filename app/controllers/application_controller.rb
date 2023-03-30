398b508e95d5770ed6764b471fde5f4f97964c72
Konrad Baumann <baumannkonrad@outlook.de>
2023-03-30 14:48:42 -0400

    bug/sign_up first_name not present

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:frist_name, :last_name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:frist_name, :last_name, :email])
